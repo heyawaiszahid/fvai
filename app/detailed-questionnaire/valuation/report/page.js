6e065e615564900b355f8aec4325e88a6556c920
Awais Zahid <mailforawais@.com>
2025-02-03 05:31:37 +0500

    report

--- app/detailed-questionnaire/valuation/report/page.js
+++ app/detailed-questionnaire/valuation/report/page.js
@@ -7,22 +7,21 @@ export default function Report() {
     <>
       <Header />
       <main className="py-4 lg:py-8">
-        <section className="text-center mb-8 lg:mb-10">
+        <section className="mb-8 lg:mb-10 text-center">
           <div className="container mx-auto px-6">
-            <div className="text-[33px] lg:text-5xl leading-[48px] font-bold mb-6 lg:mb-8">
+            <div className="mb-6 lg:mb-8 text-[33px] leading-[48px] font-bold lg:text-5xl">
               Get Your <span className="text-primary">Expert Reviewed</span> Report <br className="lg:hidden" /> in 48 hours
             </div>
-            <div className="text-base lg:text-[27px] lg:leading-10 text-text-secondary">
+            <div className="text-base text-text-secondary lg:text-[27px] lg:leading-10">
               Based on your questionnaire responses, we'll create a customized, investor-
               <br className="hidden lg:block" />
-              ready report signed off by a Chartered Financial Analyst (CFA) everything you <br className="hidden lg:block" />
-              need to pitch with confidence
+              ready report signed off by a Chartered Financial Analyst (CFA)— everything you <br className="hidden lg:block" />
+              need to pitch with confidence.
             </div>
           </div>
         </section>
-
         <section className="mb-8 lg:mb-10 lg:bg-text-primary lg:text-background-paper">
-          <div className="container mx-auto px-6 lg:py-8 lg:max-w-5xl">
+          <div className="container mx-auto px-6 lg:max-w-5xl lg:py-8">
             <div className="flex flex-col gap-8 lg:gap-16">
               {[
                 {
@@ -42,11 +41,12 @@ export default function Report() {
                 },
               ].map(({ icon, title, description }, index) => (
                 <div key={index} className="flex gap-2 lg:gap-4">
-                  <div className="bg-primary-12p lg:bg-primary-30p rounded-full w-12 h-12 lg:w-16 lg:h-16 flex justify-center items-center shrink-0">
+                  <div className="flex items-center justify-center shrink-0 w-12 h-12 rounded-full bg-primary-12p lg:w-16 lg:h-16 lg:bg-primary-30p">
                     <Image src={`/icon-${icon}.svg`} width={28} height={28} alt="" className="lg:w-10 lg:h-10" />
                   </div>
                   <div className="text-[15px] lg:text-[23px] lg:leading-snug">
-                    <span className="font-bold lg:block">{title}</span> {title === "CFA-Signed Report: " && <br className="lg:hidden" />}
+                    <span className="font-bold lg:block">{title}</span>
+                    {title === "CFA-Signed Report: " && <br className="lg:hidden" />}
                     <span className="text-text-secondary lg:text-background-paper">{description}</span>
                   </div>
                 </div>
@@ -54,15 +54,19 @@ export default function Report() {
             </div>
           </div>
         </section>
-        <section className="text-center mb-12">
+        <section className="mb-12 text-center">
           <div className="container mx-auto px-6">
-            <div className="text-[19px] lg:text-[27px] italic mb-8 lg:mb-14 leading-8 lg:mx-auto">
+            <div className="mb-8 leading-8 italic lg:mb-14 lg:mx-auto lg:text-[27px] text-[19px]">
               With 100+ valuation reports delivered in the last year, we've <br className="hidden lg:block" />
-              helped startups raise millions in funding
+              helped startups raise millions in funding.
             </div>
+          </div>
+        </section>
+        <section className="mb-12 text-center">
+          <div className="container mx-auto px-6">
             <Link
               href="/detailed-questionnaire/valuation/report/payment"
-              className="bg-primary block text-2xl text-background-paper text-center font-bold rounded-full p-4 max-w-md mx-auto mb-6"
+              className="mx-auto mb-6 block max-w-md rounded-full bg-primary p-4 text-center text-2xl font-bold text-background-paper"
             >
               Get My Full Valuation <br /> Report for $500
             </Link>
