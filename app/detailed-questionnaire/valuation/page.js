59346a7187b2f6c8c8b13da5f8a9eadccc2431c9
Awais Zahid <mailforawais@.com>
2025-02-08 03:00:37 +0500

    image 3

--- app/detailed-questionnaire/valuation/page.js
+++ app/detailed-questionnaire/valuation/page.js
@@ -11,7 +11,7 @@ export default function Valuation() {
       <Header />
       <main>
         <div className="container pb-16">
-          <section className="lg:grid grid-cols-2 gap-8 mb-4 lg:mb-12">
+          <section className="lg:grid grid-cols-2 gap-12 mb-4 lg:mb-12">
             <div className="text-center lg:text-left lg:pt-[82px]">
               <div className="text-[27px] lg:text-[47px] leading-[40px] lg:leading-[64px] text-text-primary lg:text-text-secondary mb-6 lg:mb-8">
                 Your Pre-Money <br /> Valuation is: <span className="text-primary font-bold">$6.2M</span>
@@ -27,13 +27,22 @@ export default function Valuation() {
               <div className="text-[19px] leading-[32px] mb-2 lg:mb-5">
                 We've helped startups raise millions in funding
               </div>
-              <Image src="/crypto-trading.png" width={248} height={100} alt="" className="mx-auto lg:hidden" />
+              <div className="lg:hidden px-[13vw]">
+                <Image src="/crypto-trading.png" width={248} height={100} alt="" layout="responsive" loading="eager" />
+              </div>
               <div className="hidden lg:block">
                 <Retry href="/detailed-questionnaire">Start Over</Retry>
               </div>
             </div>
             <div className="hidden lg:flex justify-end pt-1">
-              <Image src="/crypto-trading-desktop.png" width={675} height={633} alt="" />
+              <Image
+                src="/crypto-trading-desktop.png"
+                width={675}
+                height={633}
+                alt=""
+                layout="responsive"
+                loading="eager"
+              />
             </div>
           </section>
           <section className="text-center">
