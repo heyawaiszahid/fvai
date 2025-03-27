6a15a7a1c22281a857784c48cd849df7d5fccf0e
Awais Zahid <mailforawais@.com>
2025-03-28 02:03:18 +0500

    fixed! initial valuation loading delay

--- app/initial-questions/valuation/page.js
+++ app/initial-questions/valuation/page.js
@@ -1,30 +1,13 @@
-"use client";
-
 import CTA from "@/components/CTA";
 import Header from "@/components/Header";
 import Checkmark from "@/components/icons/Checkmark";
+import InitialValuation from "@/components/InitialValuation";
 import Retry from "@/components/Retry";
 import Retry2 from "@/components/Retry2";
 import Sidebar from "@/components/Sidebar";
-import spreadsheet from "@/lib/spreadsheet.json";
-import { getCookie } from "cookies-next";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 export default function Valuation() {
-  const [appData, setAppData] = useState(null);
-
-  useEffect(() => {
-    const storedData = getCookie("appData");
-    if (storedData) setAppData(JSON.parse(storedData));
-  }, []);
-
-  if (!appData) return;
-
-  const { region, industry, stage } = appData.initialQuestions;
-
-  const range = spreadsheet.structuredData?.[region]?.[industry]?.[stage] || [null, null];
-
   return (
     <>
       <div className="lg:hidden">
@@ -45,13 +28,7 @@ export default function Valuation() {
                 />
               </div>
               <div className="text-center lg:pt-20">
-                <h1 className="text-[27px] lg:text-[47px] text-text-secondary leading-[40px] lg:leading-[64px] mb-1">
-                  {stage} Stage {industry} startups in {region} have{" "}
-                  <span className="text-primary-dark font-bold">pre-money valuations</span> range between :
-                </h1>
-                <p className="text-[39px] font-bold text-text-secondary mb-5 lg:mb-8">
-                  ${range[0] || 0}M - ${range[1] || 0}M
-                </p>
+                <InitialValuation />
                 <CTA href="/detailed-questionnaire">
                   Refine My Valuation <br />
                   Start Scorecard
@@ -77,14 +54,8 @@ export default function Valuation() {
 
           <main className="pt-12 bg-custom-gradient-2">
             <div className="container px-[6vw] relative z-10">
-              <h1 className="text-[47px] text-text-secondary leading-[64px] mb-12 text-center">
-                {stage} Stage {industry} startups in {region} have{" "}
-                <span className="text-primary-dark font-bold">pre-money valuations</span> range <br /> between:{" "}
-                <span className="text-text-secondary font-bold">
-                  ${range[0] || 0}M - ${range[1] || 0}M
-                </span>
-              </h1>
               <div className="text-center">
+                <InitialValuation />
                 <CTA href="/detailed-questionnaire" className="mb-20">
                   Refine My Valuation - <br /> Start Scorecard
                 </CTA>
