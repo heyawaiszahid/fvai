74cd3c5b05568b049c4afad033fccd3b29d0a74b
Awais Zahid <mailforawais@.com>
2025-04-02 09:27:57 +0500

    updated initial valuation

--- components/InitialValuation.jsx
+++ components/InitialValuation.jsx
@@ -38,23 +38,37 @@ const InitialValuation = () => {
   return (
     <>
       <div className="lg:hidden">
-        <h1 className="text-[27px] lg:text-[47px] text-text-secondary leading-[40px] lg:leading-[64px] mb-1">
-          {stage} Stage {industry} startups in {region} have{" "}
-          <span className="text-primary-dark font-bold">pre-money valuations</span> range between :
-        </h1>
-        <p className="text-[39px] font-bold text-text-secondary mb-5 lg:mb-8">
-          ${range[0] || 0}M - ${range[1] || 0}M
-        </p>
+        {range[0] === null ? (
+          <h1 className="text-[27px] text-text-secondary leading-[40px] mb-5">
+            Fundraising data is <span className="text-primary-dark font-bold">not available</span> for this region.
+          </h1>
+        ) : (
+          <>
+            <h1 className="text-[27px] text-text-secondary leading-[40px] mb-1">
+              {stage} Stage {industry} startups in {region} have{" "}
+              <span className="text-primary-dark font-bold">pre-money valuations</span> range between :
+            </h1>
+            <p className="text-[39px] font-bold text-text-secondary mb-5">
+              ${range[0] || 0}M - ${range[1] || 0}M
+            </p>
+          </>
+        )}
       </div>
 
       <div className="hidden lg:block">
-        <h1 className="text-[47px] text-text-secondary leading-[64px] mb-12 text-center">
-          {stage} Stage {industry} startups in {region} have{" "}
-          <span className="text-primary-dark font-bold">pre-money valuations</span> range <br /> between:{" "}
-          <span className="text-text-secondary font-bold">
-            ${range[0] || 0}M - ${range[1] || 0}M
-          </span>
-        </h1>
+        {range[0] === null ? (
+          <h1 className="text-[47px] text-text-secondary leading-[64px] mb-12 text-center">
+            Fundraising data is <span className="text-primary-dark font-bold">not available</span> for this region.
+          </h1>
+        ) : (
+          <h1 className="text-[47px] text-text-secondary leading-[64px] mb-12 text-center">
+            {stage} Stage {industry} startups in {region} have{" "}
+            <span className="text-primary-dark font-bold">pre-money valuations</span> range <br /> between:{" "}
+            <span className="text-text-secondary font-bold">
+              ${range[0] || 0}M - ${range[1] || 0}M
+            </span>
+          </h1>
+        )}
       </div>
     </>
   );
